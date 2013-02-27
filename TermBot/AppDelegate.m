91fdb1778d3cc1c3f5597fa27c525e891331fdaf
martind <martin@dekstop.de>
2013-02-27 20:43:14 +0000

    Initial Commit

---
+++ TermBot/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  TermBot
+//
+//  Created by mongo on 27/02/2013.
+//  Copyright (c) 2013 martind. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ TermBot/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TermBot
+//
+//  Created by mongo on 27/02/2013.
+//  Copyright (c) 2013 martind. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
